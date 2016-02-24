bde88cc62d4ee522c0eeaef3236c8f94aeabd4bc
Marco Rojas <marco.rojas@alumno.buap.mx>
2016-02-24 19:00:52 +0000

    typo

--- routes/routes_www.js
+++ routes/routes_www.js
@@ -12,18 +12,7 @@ module.exports = (function (app){
 
 	.get(function (request , response){
 
-<<<<<<< HEAD
 		response.render('index');
-=======
-		if (request.session._id){
-				response.render('index')
-			}
-			else{
-				request.session.destroy(function (err){
-				response.redirect('/');
-				})
-			}
->>>>>>> 6136271df7498c0cf721b329884d744bb2f7e58d
 
 	});
 
