842349ade2e5abe31bd474ae99090be6d91038bf
Xchel_Sanchez <xchelsvz@gmail.com>
2016-03-02 16:17:00 -0600

    POST Function to Ficha de Identificacion and Accordion After Post Functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,12 +26,14 @@ $(document).ready(function(){
 	$('#showModal').click(function(){
 		$('#alertModal').modal('show');
 	});
+
+	$('#btnSaveFI').click(function(){$('#Collapsible').collapse('hide');});
 });
 
 /*************** Angular Modules *************/
 /*********************************************/
 
-var app = angular.module('Estoma',[/*'ngRoute', 'ngCookies'*/]);
+var app = angular.module('Estoma',['ngRoute', 'ngCookies']);
 
 app.controller ('indexController',function  ($scope , $http, $window) {
 
@@ -73,6 +75,75 @@ app.controller ('indexController',function  ($scope , $http, $window) {
 			divisor.removeClass('has-success');
 			divisor.addClass('has-error');
 		}
+	};
+
+	$scope.full=false;
+	$scope.saveFicha = function (){
+
+		var vacunasArray = [];
+		for (var w = 0; w < $scope.array.length; w++) {
+
+			var vac = angular.element(document.querySelector('#vacuna'+w));
+			vacunasArray.push(vac.val());
+			
+		}
+
+		var ficha_Identificacion = {
+
+			nombre:{
+	            nombre: $scope.input.name,
+	            apellidoP: $scope.input.apellidoP,
+	            apellidoM: $scope.input.apellidoM
+	         },
+	         fechaNac: $scope.input.fechaNac,
+	         sexo: $scope.input.genero,
+	         lugarNac: $scope.input.lugarNac,
+	         ocupacion: $scope.input.ocupacion,
+	         escolaridad: $scope.input.escolaridad,
+	         edoCivil: $scope.input.edoCivil,
+	         domicilio: {
+	             calle: $scope.input.calle,
+	             numeroInt: $scope.input.interior,
+	             numeroExt: $scope.input.exterior,
+	             cp: $scope.input.cp,
+	             colonia: $scope.input.colonia,
+	             municipio: $scope.input.municipio,
+	             estado: $scope.input.estado
+	         },
+	         telefono: {
+	         	telFijo: $scope.input.telFijo,
+	            telMovil: $scope.input.telMovil,
+	            telOficina: $scope.input.telOficina
+
+	         },
+	         curp: $scope.input.curp, 
+	         email: $scope.input.email,
+	         ingresos: $scope.input.ingresos,
+	         sangre: $scope.input.sangre,
+	         vacunas:vacunasArray
+
+		};
+		console.log(ficha_Identificacion);
+
+		$http.post('/fichaDeIdentificacion',ficha_Identificacion).success(function (data, status, headers, config){
+					
+			var panel = angular.element(document.querySelector('#FIPanel'));
+			
+
+			panel.removeClass('panel-default');
+			panel.addClass('panel-success');
+			$scope.full=true;
+
+			console.log($scope.full);
+
+					
+		})
+		.error(function (){
+
+			alert('AJAX error in post fichaDeIdentificacion');
+					
+		});
+
 	};
 });
 
